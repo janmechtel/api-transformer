80731d4be0698fc3bbfebc1942fce88dbdee95d7
Jan Mechtel <jme@templafy.com>
2022-05-15 21:23:05 +0200

    make outgoing request with fetch configured via parameter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.sendStatus(200);
+var route = {
+    fetchRequest: {
+        url: 'https://api.sampleapis.com/wines/reds',
+        options: {}
+    }
+}
+
+function getData(url, options) {
+    fetch(url, options)
+        .then(inResponse => inResponse.json())
+        .then(function(data) {
+            return data;
+        });
+}
+
+app.get('*', function(inRequest, outResponse) {
+    console.log(inRequest);
+    outResponse.status = 200;
+    var inResponseData = getData(route.fetchRequest.url, route.fetchRequest.options);
+    console.log(inResponseData[1]);
+    outResponse.send(inResponseData);
+
 });
 
 app.listen(PORT, function() {
     console.log('Server is running on PORT:', PORT);
 });
+
+// getData(route.fetchRequest.url, route.fetchRequest.options);
